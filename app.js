8a18f3a8bedda841f91611df66c9254d4be0f05c
root <nmidsk28@naver.com>
2020-01-14 07:52:23 +0000

    update matching system

--- app.js
+++ app.js
@@ -61,14 +61,47 @@ io.on('connection', function(socket){
   socket.on('enterRoom', function(data){
     userId = data.userId;
     userPosi = data.userPosi;
-    roomNumber = data.roomNumber;
+    roomNumbers_ = data.roomNumbers;
+    roomNumbers_ = roomNumbers_.substring(1, roomNumbers_.length-1);
+    roomNumbers = roomNumbers_.split(', ');
     
-    rooms[roomNumber].addUser(userId, userPosi);
+    for(var i = 0; i < roomNumbers.length; i++){
+      rooms[roomNumbers[i]].addUser(userId, userPosi);
 
-    if(rooms[roomNumber].detectMatched() == true){
-      var matched = rooms[roomNumber].removeSet();
-      io.sockets.emit("matchComplete", matched);
+      if(rooms[roomNumbers[i]].detectMatched() == true){
+        var matched = rooms[roomNumbers[i]].removeSet();
+        io.sockets.emit("matchComplete", matched);
+      }
+  
+      top_ = rooms[roomNumbers[i]].gettop();
+      console.log('top : '+top_);
+      jungle_ = rooms[roomNumbers[i]].getjungle();
+      console.log('jungle : '+jungle_);
+      mid_ = rooms[roomNumbers[i]].getmid();
+      console.log('mid : '+mid_);
+      bottom_ = rooms[roomNumbers[i]].getbottom();
+      console.log('bottom : '+bottom_);
+      support_ = rooms[roomNumbers[i]].getsupport();
+      console.log('support : '+support_);
     }
+
+    // rooms[roomNumber].addUser(userId, userPosi);
+
+    // if(rooms[roomNumber].detectMatched() == true){
+    //   var matched = rooms[roomNumber].removeSet();
+    //   io.sockets.emit("matchComplete", matched);
+    // }
+
+    // top_ = rooms[roomNumber].gettop();
+    // console.log('top : '+top_);
+    // jungle_ = rooms[roomNumber].getjungle();
+    // console.log('jungle : '+jungle_);
+    // mid_ = rooms[roomNumber].getmid();
+    // console.log('mid : '+mid_);
+    // bottom_ = rooms[roomNumber].getbottom();
+    // console.log('bottom : '+bottom_);
+    // support_ = rooms[roomNumber].getsupport();
+    // console.log('support : '+support_);
   });
 
   socket.on('exitRoom', function(data){
@@ -76,7 +109,19 @@ io.on('connection', function(socket){
     userPosi = data.userPosi;
     roomNumber = data.roomNumber;
 
-    rooms[roomNumber].re
+    rooms[roomNumber].removeUser(userId, userPosi);
+
+    top_ = rooms[roomNumber].gettop();
+    console.log('top : '+top_);
+    jungle_ = rooms[roomNumber].getjungle();
+    console.log('jungle : '+jungle_);
+    mid_ = rooms[roomNumber].getmid();
+    console.log('mid : '+mid_);
+    bottom_ = rooms[roomNumber].getbottom();
+    console.log('bottom : '+bottom_);
+    support_ = rooms[roomNumber].getsupport();
+    console.log('support : '+support_);
+
   });
 
   socket.on('ready', function(data){
--- models/room.js
+++ models/room.js
@@ -8,6 +8,21 @@ class Room {
         this.support = [];
         this.num = num;
     }
+    gettop(){
+        return this.top;
+    }
+    getjungle(){
+        return this.jungle;
+    }
+    getmid(){
+        return this.mid;
+    }
+    getbottom(){
+        return this.bottom;
+    }
+    getsupport(){
+        return this.support;
+    }
     
     //userId와 position을 받아서 해당 list의 맨앞에 user추가
     addUser(userId, position){
@@ -31,29 +46,29 @@ class Room {
 
     //userId와 positin을 받아서 해당 list의 user를 지움
     removeUser(userId, position){
-        console.log('removeUser :'+ this.num+2+" "+userId +" "+position);
+        console.log('removeUser :'+ (this.num+2)+" "+userId +" "+position);
         if(position === 'top'){
-            for(var i = this.top.length-1; i--;){
+            for(var i = 0; i < this.top.length; i++){
                 if(this.top[i] === userId) this.top.splice(i,1);
             }
         }
         else if(position === 'jungle'){
-            for(var i = this.jungle.length-1; i--;){
+            for(var i = 0; i < this.jungle.length; i++){
                 if(this.jungle[i] === userId) this.jungle.splice(i,1);
             }
         }
         else if(position === 'mid'){
-            for(var i = this.mid.length-1; i--;){
+            for(var i = 0; i < this.mid.length; i++){
                 if(this.mid[i] === userId) this.mid.splice(i,1);
             }
         }
         else if(position === 'bottom'){
-            for(var i = this.bottom.length-1; i--;){
+            for(var i = 0; i < this.bottom.length; i++){
                 if(this.bottom[i] === userId) this.bottom.splice(i,1);
             }
         }
         else if(position === 'support'){
-            for(var i = this.support.length-1; i--;){
+            for(var i = 0; i < this.support.length; i++){
                 if(this.support[i] === userId) this.support.splice(i,1);
             }
         }
